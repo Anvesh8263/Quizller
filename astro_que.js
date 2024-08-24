c357313ea2572c4f18c2f383e455195e7f4a53ef
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-08-24 14:04:35 +0530

    Add files via upload

---
+++ astro_que.js
@@ -0,0 +1,71 @@
+var data = [{
+	"question":"What is the primary function of the mitochondria in eukaryotic cells?",
+	"a":"Protein synthesis",
+	"b":"Energy production",
+	"c":"DNA replication",
+	"d":"Photosynthesis",
+	"ans":"b"
+},{
+	"question":"Which of the following is a characteristic of Mendelian inheritance?",
+	"a":"Codominance",
+	"b":"Incomplete dominance",
+	"c":"Segregation",
+	"d":"Epistasis",
+	"ans":"c"
+},{
+	"question":"Which part of the human brain is primarily responsible for regulating homeostasis?",
+	"a":"Cerebellum",
+	"b":"Cerebrum",
+	"c":"Hypothalamus",
+	"d":"Medulla oblongata",
+	"ans":"c"
+},{
+	"question":"In plants, the process of photosynthesis primarily occurs in which part of the plant?",
+	"a":"Roots",
+	"b":"Stems",
+	"c":"Leaves",
+	"d":"Flowers",
+	"ans":"c"
+},{
+	"question":"What is the term for a relationship between two species where one benefits and the other is neither helped nor harmed?",
+	"a":"Mutualism",
+	"b":"Commensalism",
+	"c":"Parasitism",
+	"d":"Competition",
+	"ans":"b"
+},{
+	"question":"What hormone is primarily responsible for the regulation of the menstrual cycle?",
+	"a":"Insulin",
+	"b":"Estrogen",
+	"c":"Progesterone",
+	"d":"Adrenaline",
+	"ans":"c"
+},{
+	"question":"Which of the following is a key concept of Darwin's theory of natural selection?",
+	"a":"Genetic drift",
+	"b":"Adaptation",
+	"c":"Gene flow",
+	"d":"Artificial selection",
+	"ans":"b"
+},{
+	"question":"Which type of macromolecule are enzymes classified as?",
+	"a":"Carbohydrates",
+	"b":"Lipids",
+	"c":"Proteins",
+	"d":"Nucleic acids",
+	"ans":"c"
+},{
+	"question":"Which of the following is not a characteristic of prokaryotic cells?",
+	"a":"Lack of a true nucleus",
+	"b":"Presence of ribosomes",
+	"c":"Presence of membrane-bound organelles",
+	"d":"Presence of a cell wal",
+	"ans":"c"
+},{
+	"question":"In which part of the plant does secondary growth primarily occur?",
+	"a":"Root cap",
+	"b":"Apical meristem",
+	"c":"Lateral meristem",
+	"d":"Epidermis",
+	"ans":"c"
+}]
